2bd88f38290db15f37c4e4319a3d050e67dbfb8b
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-02-01 21:33:46 +0000

    Introduce EnderEyeItemMixin

---
+++ src/main/java/io/github/ayohee/createendergateway/mixin/EnderEyeItemMixin.java
@@ -0,0 +1,8 @@
+package io.github.ayohee.createendergateway.mixin;
+
+import net.minecraft.world.item.EnderEyeItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EnderEyeItem.class)
+public interface EnderEyeItemMixin {
+}
