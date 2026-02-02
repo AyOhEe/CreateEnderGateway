41182fa9a3f4d1e2e46590ae820ee1863934fa0c
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-02-02 16:41:04 +0000

    Alter ActiveEnder Solution recipe
    No longer gives back 90% chance for nether wart. Turns out the mixer didn't like not having room.

--- src/main/java/io/github/ayohee/createendergateway/datagen/recipes/EGMixingRecipeGen.java
+++ src/main/java/io/github/ayohee/createendergateway/datagen/recipes/EGMixingRecipeGen.java
@@ -14,12 +14,11 @@ import static io.github.ayohee.createendergateway.CreateEnderGateway.MODID;
 
 public class EGMixingRecipeGen extends MixingRecipeGen {
     public final GeneratedRecipe ACTIVE_ENDER_SOLUTION = create("active_ender_solution", b -> b
-            .require(EGFluids.DORMANT_ENDER_SOLUTION.get(), 100)
+            .require(EGFluids.DORMANT_ENDER_SOLUTION.get(), 500)
             .require(Items.NETHER_WART)
             .requiresHeat(HeatCondition.HEATED)
-            .output(EGFluids.ACTIVE_ENDER_SOLUTION.get(), 100)
-            .output(0.9f, Items.NETHER_WART)
-            .duration(10)
+            .output(EGFluids.ACTIVE_ENDER_SOLUTION.get(), 500)
+            .duration(50)
     );
 
     public final GeneratedRecipe SATURATED_EYE_FRAGMENTS = create("saturated_eye_fragments", b -> b
