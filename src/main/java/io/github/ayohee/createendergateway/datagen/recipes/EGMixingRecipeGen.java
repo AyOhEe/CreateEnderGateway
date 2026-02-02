0d3500965c09506fec0ef23448cfeb39c1a33820
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-02-02 11:04:56 +0000

    Saturated Eye Fragments recipe

--- src/main/java/io/github/ayohee/createendergateway/datagen/recipes/EGMixingRecipeGen.java
+++ src/main/java/io/github/ayohee/createendergateway/datagen/recipes/EGMixingRecipeGen.java
@@ -3,6 +3,7 @@ package io.github.ayohee.createendergateway.datagen.recipes;
 import com.simibubi.create.api.data.recipe.MixingRecipeGen;
 import com.simibubi.create.content.processing.recipe.HeatCondition;
 import io.github.ayohee.createendergateway.register.EGFluids;
+import io.github.ayohee.createendergateway.register.EGItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Items;
@@ -17,6 +18,13 @@ public class EGMixingRecipeGen extends MixingRecipeGen {
             .require(Items.NETHER_WART)
             .requiresHeat(HeatCondition.HEATED)
             .output(EGFluids.ACTIVE_ENDER_SOLUTION.get(), 1000)
+    );
+
+    public final GeneratedRecipe SATURATED_EYE_FRAGMENTS = create("saturated_eye_fragments", b -> b
+            .require(EGFluids.ACTIVE_ENDER_SOLUTION.get(), 1000)
+            .require(Items.BLAZE_POWDER)
+            .require(Items.BLAZE_POWDER)
+            .output(EGItems.SATURATED_EYE_FRAGMENT, 12)
     );
 
     public EGMixingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
