5cdafdf0dbbac4ff4bf82216737533e38081276e
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-02-19 12:12:40 +0000

    GatewayBlockEntity saves linkedPos

--- src/main/java/io/github/ayohee/createendergateway/content/blockentity/GatewayBlockEntity.java
+++ src/main/java/io/github/ayohee/createendergateway/content/blockentity/GatewayBlockEntity.java
@@ -1,8 +1,11 @@
 package io.github.ayohee.createendergateway.content.blockentity;
 
 import com.simibubi.create.api.equipment.goggles.IHaveHoveringInformation;
+import net.createmod.catnip.nbt.NBTHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
@@ -14,7 +17,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import java.util.List;
 
 public class GatewayBlockEntity extends BlockEntity implements IHaveHoveringInformation {
-    private BlockPos linkedPos;
+    private BlockPos linkedPos = null;
 
     public GatewayBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -48,5 +51,25 @@ public class GatewayBlockEntity extends BlockEntity implements IHaveHoveringInfo
         );
 
         return true;
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+        if (linkedPos != null) {
+            CompoundTag asNBT = new CompoundTag();
+            asNBT.putInt("X", linkedPos.getX());
+            asNBT.putInt("Y", linkedPos.getY());
+            asNBT.putInt("Z", linkedPos.getZ());
+            tag.put("linked_pos", asNBT);
+        }
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+        if (tag.contains("linked_pos")) {
+            linkedPos = NBTHelper.readBlockPos(tag, "linked_pos");
+        }
     }
 }
